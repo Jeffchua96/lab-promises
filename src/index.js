7d8ac550424a31b95727feda1f8228c796d8e09c
Jeffchua96 <jeffersonchua@pursuit.org>
2023-03-22 19:55:55 -0400

    lab promise done

--- src/index.js
+++ src/index.js
@@ -10,7 +10,14 @@ const finder = require("./helpers/finder");
  * @param {Object} person.first - The person's first name.
  * @param {Object} person.last - The person's last name.
  */
-function logResult() {}
+function logResult(person) {
+  let promise = finder(person.first, person.last)  
+  promise.then (result => {
+    console.log(result)
+  }).catch(error => {
+    console.log(error)
+  })
+}
 
 /**
  * logTwoResults()
@@ -22,8 +29,21 @@ function logResult() {}
  * @param {Object} person1.last - The person's last name.
  * @param {Object} person2 - A person's names. In the same format as person1.
  */
-function logTwoResults() {}
+function logTwoResults(person1 , person2) {
+  let promise1 = finder(person1.first, person1.last)  
+  promise1.then (result => {
+    console.log(result)
+  }).catch(error => {
+    console.log(error)
+  })
 
+  let promise2 = finder(person2.first, person2.last)  
+  promise2.then (result => {
+    console.log(result)
+  }).catch(error => {
+    console.log(error)
+  })
+  } 
 /**
  * logThreeResultsCities()
  * ========================
@@ -34,8 +54,21 @@ function logTwoResults() {}
  * @param {Object} person1.last - The person's last name.
  * @param {Object} person2 - A person's names. In the same format as person1.
  * @param {Object} person3 - A person's names. In the same format as person1.
- */
-function logThreeResultsCities() {}
+ *
+**/
+
+function logThreeResultsCities(person1, person2, person3) {
+for (let i = 0; i < arguments.length; i++) {
+  let promise =finder(arguments[i].first, arguments[i].last)
+  promise.then(result => {
+  console.log(result[0].city)
+  }).catch(error => {
+  console.log(error)
+  })   
+  }
+  }
+
+
 
 // Do not change any of the code below this line.
 module.exports = {
